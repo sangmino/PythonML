a5dc74152ccaa7b5b381c9e8853fa345db966411
Simon Oh <sangmin92@gmail.com>
2020-09-07 21:53:24 +0900

    Create Sample Snippets.py

---
+++ Code/Sample Snippets.py
@@ -0,0 +1,277 @@
+# coding: utf-8
+
+
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from pydotplus import graph_from_dot_data
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+#----------------------------------------------------------#
+# region : Perceptron Algorithm
+
+## Choosing a classification algorithm
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+print('Class labels:', np.unique(y))
+
+# Splitting data into 70% training and 30% test data:
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+print('Labels counts in y:', np.bincount(y))
+print('Labels counts in y_train:', np.bincount(y_train))
+print('Labels counts in y_test:', np.bincount(y_test))
+
+# Standardizing the features:
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# Training a perceptron via scikit-learn
+ppn = Perceptron(eta0 = 0.1, random_state = 1) # eta0 is the learning rate
+ppn.fit(X_train_std, y_train)
+y_pred = ppn.predict(X_test_std)
+print('Misclassified examples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+print('Accuracy: %.3f' % ppn.score(X_test_std, y_test))
+
+
+# Plot the decision regions of the perceptron algorithm
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    if test_idx:
+
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0], X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+plt.close()
+
+# endregion
+
+#----------------------------------------------------------#
+# region : Logistic regression
+
+## Training a logistic regression model with scikit-learn
+lr = LogisticRegression(C = 100.0, random_state = 1, solver = 'lbfgs', multi_class = 'ovr')
+lr.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=lr, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+lr.predict_proba(X_test_std[:3, :])
+lr.predict_proba(X_test_std[:3, :]).sum(axis=1)
+lr.predict_proba(X_test_std[:3, :]).argmax(axis=1)
+lr.predict(X_test_std[:3, :])
+lr.predict(X_test_std[0, :].reshape(1, -1))
+
+# endregion
+
+#----------------------------------------------------------#
+# region : SVM for Maximum Margin Classification
+
+## Basic Training of an SVM
+svm = SVC(kernel = 'linear', C = 1.0, random_state = 1)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, 
+                      y_combined,
+                      classifier=svm, 
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+## Alternative implementations in scikit-learn
+ppn = SGDClassifier(loss='perceptron')
+lr = SGDClassifier(loss='log')
+svm = SGDClassifier(loss='hinge')
+
+## Solving non-linear problems using a kernel SVM
+
+# Generate a simple dataset
+np.random.seed(1)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+# Using the kernel trick to find separating hyperplanes in higher dimensional space
+# Gamma = 0.10
+svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0) # gamma = cut-off parameter
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor,
+                      classifier=svm)
+
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# Gamma = 0.20
+svm = SVC(kernel='rbf', random_state=1, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# endregion
+
+#----------------------------------------------------------#
+# region : Decision Tree Learning
+
+# Building a decision tree
+tree_model = DecisionTreeClassifier(criterion='gini', 
+                                    max_depth=4, 
+                                    random_state=1)
+tree_model.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+
+# Plotting the classification
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=tree_model,
+                      test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# Plotting the tree
+tree.plot_tree(tree_model)
+plt.show()
+
+# Create the image of the decision tree in local PNG
+dot_data = export_graphviz(tree_model,
+                           filled=True, 
+                           rounded=True,
+                           class_names=['Setosa', 
+                                        'Versicolor',
+                                        'Virginica'],
+                           feature_names=['petal length', 
+                                          'petal width'],
+                           out_file=None) 
+graph = graph_from_dot_data(dot_data) 
+graph.write_png('tree.png') 
+
+# endregion
+
+#----------------------------------------------------------#
+# region : Random Forest
+
+## Combining weak to strong learners via random forests
+forest = RandomForestClassifier(criterion='gini',
+                                n_estimators=25, 
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# endregion
+
+#----------------------------------------------------------#
+# region : K-Nearest Neighbors (KNN)
+
+## KNN using Euclidean distance metric
+knn = KNeighborsClassifier(n_neighbors=5, 
+                           p=2, 
+                           metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, 
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('images/03_24.png', dpi=300)
+plt.show()
+
+
+
+# # Summary
+
+# ...
+
+# ---
+# 
+# Readers may ignore the next cell.
+
+
+
+
